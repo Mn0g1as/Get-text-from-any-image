8f3eb60869fc9765c67afce68dd1380025fd7857
GoNz0 | Mn0g1as <128939384+Mn0g1as@users.noreply.github.com>
2023-07-08 18:06:50 +0200

    Update text-image.py

--- text-image.py
+++ text-image.py
@@ -1,1 +1,10 @@
+# Have Fun 
+# Free code 
+# Made by Mn0g1as
 
+import pytesseract
+from PIL import image
+
+image = image.open('PATH/TO/IMAGE.png')
+text=pytesseract.image_to_string(image)
+print(text)
